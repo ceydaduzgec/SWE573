506a585f7998ccbd1dbe21d45eb1fd42ee6f13aa
Ceyda Düzgeç <cduzgec@gmail.com>
2022-10-23 13:48:48 +0300

    Create project from cookiecutter

---
+++ sole/sole/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sole.users.models import User
+from sole.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
