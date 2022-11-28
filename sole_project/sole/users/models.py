0a56fda1e612d1faa5738e9af2c1c01d81bb05c3
Ceyda Duzgec <cduzgec@gmail.com>
2022-11-28 22:33:17 +0300

    Fix validationerror for user

--- sole_project/sole/users/models.py
+++ sole_project/sole/users/models.py
@@ -1,11 +1,25 @@
 from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.validators import RegexValidator
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-    username = models.CharField(_("Username"), max_length=150, unique=True, blank=False)
+    username = models.CharField(
+        _("Username"),
+        max_length=150,
+        unique=True,
+        blank=False,
+        validators=[
+            RegexValidator(
+                r"^[a-z0-9._]{3,}$",  # Only lowercase English letters, period and underscore characters are allowed. Minimum length is three characters.
+                _(
+                    "Username is invalid. Only lowercase English letters, period and underscore characters are allowed. Minimum length is three characters."
+                ),
+            )
+        ],
+    )
     email = models.EmailField(_("Email"), unique=True, blank=False)
     bio = models.TextField(_("Bio"), blank=True, null=False)
 
@@ -14,3 +28,7 @@ class User(AbstractUser):
     def get_absolute_url(self):
         # TODO: filter by username
         return reverse("glimpses:list", args=[self.username])
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
