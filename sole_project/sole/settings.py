24f5c43e2cef77e91e85a1b265409cfb6d3a307f
Ceyda Duzgec <cduzgec@gmail.com>
2022-12-25 13:55:20 +0300

    Add default value for tests

--- sole_project/sole/settings.py
+++ sole_project/sole/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
-# CHANGE THIS AFTER DONE
+DEBUG = os.environ.get("DEBUG", default=True)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="k8pmtm-1lktmj_x^axio41cyve!q*rrt3r)+b(-zw_^&qp4=ib")
+# default for development and test purposes
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 if not DEBUG:
